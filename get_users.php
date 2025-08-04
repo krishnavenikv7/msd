008b3c42d62ed22391a057fbd5e918c1ccd0c090
krishnavenikv7 <231fa04835@gmail.com>
2025-08-04 17:52:39 +0530

    Add files via upload

---
+++ get_users.php
@@ -0,0 +1,13 @@
+<?php
+include "db.php"; 
+
+$sql = "SELECT * FROM users"; 
+$result = $conn->query($sql);
+
+$data = array();
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+?>
